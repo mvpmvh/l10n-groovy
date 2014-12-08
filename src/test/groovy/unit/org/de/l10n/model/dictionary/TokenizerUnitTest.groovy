a0e4bd053b2801352043b2df432b8b2920f355ae
Michael Hatch <mvhatch@gmail.com>
2014-12-08 09:57:49 -0600

    Added a few more unit tests to TokenizerUnitTest

--- src/test/groovy/unit/org/de/l10n/model/dictionary/TokenizerUnitTest.groovy
+++ src/test/groovy/unit/org/de/l10n/model/dictionary/TokenizerUnitTest.groovy
@@ -13,8 +13,8 @@ class TokenizerUnitTest {
     @Test
     void "tokenize should parse all tokens" () {
 
-        def content = "The cat is %{color} and has p('eye || eyes', n)"
-        def tokens = Tokenizer.tokenize(content)
+        def phrase = "The cat is %{color} and has p('eye || eyes', n)"
+        def tokens = Tokenizer.tokenize(phrase)
         def expectedTokens = [
                 new TextToken("The cat is "),
                 new ExpressionToken("%{color}"),
@@ -22,6 +22,36 @@ class TokenizerUnitTest {
                 new PluralToken("p('eye || eyes', n)"),
                 new TextToken("")
         ]
+
+        assert expectedTokens == tokens
+    }
+
+    @Test
+    void "tokenize should return plural token when plural grammar applied" () {
+
+        def phrase = "p('height || heights)"
+        def tokens = Tokenizer.tokenize(phrase)
+        def expectedTokens = [new PluralToken("p('height || heights)")]
+
+        assert expectedTokens == tokens
+    }
+
+    @Test
+    void "tokenize should return expression token when expression grammar applied" () {
+
+        def phrase = "%{color}"
+        def tokens = Tokenizer.tokenize(phrase)
+        def expectedTokens = [new ExpressionToken("%{color}")]
+
+        assert expectedTokens == tokens
+    }
+
+    @Test
+    void "tokenize return text token when plain text grammar applied" () {
+
+        def phrase = "Hello, World!"
+        def tokens = Tokenizer.tokenize(phrase)
+        def expectedTokens = [new TextToken("Hello, World!")]
 
         assert expectedTokens == tokens
     }
